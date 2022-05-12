5cf4f9b1621d1af7782c934d1ec21bb1a198a252
behnazafsheh <behnazfaheh1992@gmail.com>
2022-05-12 16:35:25 +0430

    dark mood added

--- js/script.js
+++ js/script.js
@@ -13,3 +13,28 @@ var swiper = new Swiper(".mySwiper", {
     prevEl: ".swiper-button-prev",
   },
 });
+
+// dark mood
+var darkMode = document.getElementById("dark-mode");
+var imgLogo = document.getElementById("header-logo");
+var footer = document.getElementById("footer");
+var footerHeader1 = document.getElementById("footer-header1");
+var footerHeader2 = document.getElementById("footer-header2");
+var footerHeader3 = document.getElementById("footer-header3");
+var footerHeader4 = document.getElementById("footer-header4");
+
+darkMode.onclick = function () {
+  document.body.classList.toggle("dark-theme");
+  if (document.body.classList.contains("dark-theme")) {
+    imgLogo.src = "images/logo_light.png";
+    footer.style.backgroundColor = "black";
+    footerHeader1.style.color = "white";
+    footerHeader2.style.color = "white";
+    footerHeader3.style.color = "white";
+    footerHeader4.style.color = "white";
+
+  }
+  else{
+    imgLogo.src = "images/logo_dark.png";
+  }
+};
--- script.js
+++ script.js
@@ -18,15 +18,15 @@
 
 
 
-var navbar = document.querySelector(".header-nav-bar ");
-var menu = document.querySelector(".menu");
-var sticky = header.offsetTop;
+// var navbar = document.querySelector(".header-nav-bar ");
+// var menu = document.querySelector(".menu");
+// var sticky = header.offsetTop;
 
-window.onscroll = function () {
-    if (window.pageYOffset >= menu.offsetTop) {
-        navbar.classList.add("sticky");
-    }
-    else{
-        navbar.classList.remove("sticky");
-    }
-}
+// window.onscroll = function () {
+//     if (window.pageYOffset >= menu.offsetTop) {
+//         navbar.classList.add("sticky");
+//     }
+//     else{
+//         navbar.classList.remove("sticky");
+//     }
+// }
