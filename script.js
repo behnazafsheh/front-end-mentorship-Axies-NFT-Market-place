247306fb53347299087e6dcaebd16b12318275f1
behnazafsheh <behnazfaheh1992@gmail.com>
2022-05-12 13:47:19 +0430

    sticky header fixed

---
+++ script.js
@@ -0,0 +1,32 @@
+
+// window.onscroll = function() {myFunction()};
+
+
+// var header = document.querySelector(".menu-container");
+// var menu = document.querySelector(".menu");
+// var sticky = header.offsetTop;
+
+
+
+// function myFunction() {
+//   if (window.pageYOffset > sticky) {
+//     header.classList.add("sticky");
+//   } else {
+//     header.classList.remove("sticky");
+//   }
+// }
+
+
+
+var navbar = document.querySelector(".header-nav-bar ");
+var menu = document.querySelector(".menu");
+var sticky = header.offsetTop;
+
+window.onscroll = function () {
+    if (window.pageYOffset >= menu.offsetTop) {
+        navbar.classList.add("sticky");
+    }
+    else{
+        navbar.classList.remove("sticky");
+    }
+}
